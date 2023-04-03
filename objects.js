f779b1feb3c44ffc216fde69fcac32df13cc477c
Charlotte <charlotte.bourg@gmail.com>
2023-04-03 15:36:30 -0700

    objects

--- objects.js
+++ objects.js
@@ -2,10 +2,29 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const word_counts = {};
+  for (const word of phrase.split(" ")){
+    if (word_counts[word]){
+      word_counts[word]++;
+    }
+    else{
+      word_counts[word] = 1;
+    }
+  }
+  return word_counts; 
+
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  if (!melonPrices.hasOwnProperty(price)){
+    return; 
+  }
+  return melonPrices[price].sort();
 }
