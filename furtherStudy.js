2ed133577b66f20cb604ca629f50ab8258ccbcab
Charlotte <charlotte.bourg@gmail.com>
2023-04-03 16:40:14 -0700

    cleaned up comments in further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -17,11 +17,8 @@ function wordsInCommon(words1, words2) {
 function kidsGame(names) {
   let output = [];
   output.push(names.shift());
-  //console.log(output);
   let firstLetterToWords = {};
-  //console.log(firstLetterToWords);
   for (const name of names){
-    //console.log(name);
     if (!firstLetterToWords[name[0]]){
       firstLetterToWords[name[0]] = [name];
     }
@@ -29,21 +26,13 @@ function kidsGame(names) {
       firstLetterToWords[name[0]].push(name)
     }
   }
-  //console.log(firstLetterToWords);
   while (true){
     let prevWord = output[(output.length-1)];
-    //console.log(prevWord);
-    //if (!prevWord)
-    //console.log(output);
-    //console.log(firstLetterToWords);
     let startLetter = prevWord[prevWord.length-1];
-    //console.log(startLetter);
 
     if (!firstLetterToWords[startLetter] || firstLetterToWords[startLetter].length === 0){
       break; 
     }
-    //names =["bagon", "baltoy", "yamask", "starly", "nosepass", "kalob", "nicky", "booger"]
-
     let word = firstLetterToWords[startLetter].shift();
     output.push(word); 
   }
