d1088a881ec2654af2056a32ed4c5fde4fe532cb
Charlotte <charlotte.bourg@gmail.com>
2023-04-03 12:51:04 -0700

    updated arrays

--- arrays.js
+++ arrays.js
@@ -2,15 +2,27 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (let i = 0; i < items.length; i += 1) {
+    console.log(`${items[i]} ${i}`);
+  }
 }
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (let i = 0; i < items.length; i += 1){
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b);
+  let sortedNItems = sortedItems.slice(0, n);
+  sortedNItems.reverse(); 
+  console.log(sortedNItems);
 }
